086876c197cec682ba202168e2260eda4a942be9
Jianming Chen <loafbaker@hotmail.com>
2016-03-11 14:02:03 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+django_ecommerce2
+=================
+
+An improved e-commerce app built within django framework
+
+Run in django 1.9.4 and python 2.7.6
+
+# Setup
+
+Install all the required libraries
+
+    pip install -r requirements.txt
+
+Rebuild database
+
+    rm db.sqlite3
+    python manage.py migrate
+    python manage.py createsuperuser
+
+Collect static files
+
+    python manage.py collectstatic
+
+Run web server
+
+    python manage.py runserver
+
+Finally, you can view the web app with your local browser by accessing http://localhost:8000/
+
---
+++ django_ecommerce2/urls.py
@@ -0,0 +1,29 @@
+"""django_ecommerce2 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from newsletter import views as newsletter_views
+from . import views as main_views
+
+urlpatterns = [
+    url(r'^$', newsletter_views.home, name='home'),
+    url(r'^contact/$', newsletter_views.contact, name='contact'),
+    url(r'^about/$', main_views.about, name='about'),
+
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('registration.backends.default.urls')),
+]
